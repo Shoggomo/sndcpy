df3266a4e979e8719f34838e33478226c2aa8d43
Benedikt Strasser <andy-hot@web.de>
2023-08-25 14:17:59 +0200

    Applied PR#124 and restart program, when device disconnects

--- sndcpy.bat
+++ sndcpy.bat
@@ -4,6 +4,8 @@ if not defined VLC set VLC="C:\Program Files\VideoLAN\VLC\vlc.exe"
 if not defined SNDCPY_APK set SNDCPY_APK=sndcpy.apk
 if not defined SNDCPY_PORT set SNDCPY_PORT=28200
 
+:start
+
 if not "%1"=="" (
     set serial=-s %1
     echo Waiting for device %1...
@@ -12,20 +14,40 @@ if not "%1"=="" (
 )
 
 %ADB% %serial% wait-for-device || goto :error
-%ADB% %serial% install -t -r -g %SNDCPY_APK% || (
-    echo Uninstalling existing version first...
-    %ADB% %serial% uninstall com.rom1v.sndcpy || goto :error
-    %ADB% %serial% install -t -g %SNDCPY_APK% || goto :error
-)
-%ADB% %serial% shell appops set com.rom1v.sndcpy PROJECT_MEDIA allow
-%ADB% %serial% forward tcp:%SNDCPY_PORT% localabstract:sndcpy || goto :error
-%ADB% %serial% shell am start com.rom1v.sndcpy/.MainActivity || goto :error
 
-timeout 2
+:checkIfAppIsInstalled
+	%ADB% shell pm list packages | findstr /C:"com.rom1v.sndcpy" > temp.txt
+	FindStr /C:"com.rom1v.sndcpy" temp.txt >Nul && (goto :startForwarding) || goto :installApp
+:end
 
-echo Playing audio...
-%VLC% -Idummy --demux rawaud --network-caching=0 --play-and-exit tcp://localhost:%SNDCPY_PORT%
-goto :EOF
+:installApp
+	%ADB% %serial% install -t -r -g %SNDCPY_APK% || (
+		echo Uninstalling existing version first...
+		%ADB% %serial% uninstall com.rom1v.sndcpy || goto :error
+		%ADB% %serial% install -t -g %SNDCPY_APK% || goto :error
+	)
+:end
+
+:startForwarding
+	%ADB% %serial% forward tcp:%SNDCPY_PORT% localabstract:sndcpy || goto :error
+	%ADB% %serial% shell am start com.rom1v.sndcpy/.MainActivity || goto :error
+:end
+
+:: This method doesn't count that the screen could be skipped
+:checkAllowed
+	::Recommended delay time for the popup to appear
+	timeout 5
+	
+	%ADB% shell dumpsys gfxinfo com.rom1v.sndcpy > temp.txt
+	FindStr /C:"com.rom1v.sndcpy/com.rom1v.sndcpy.MainActivity/android.view.ViewRootImpl" temp.txt >Nul && (goto :checkAllowed) || goto :playAudio
+:end
+
+:playAudio
+	echo Playing audio...
+	%VLC% -Idummy --demux rawaud --network-caching=50 --play-and-exit tcp://localhost:%SNDCPY_PORT%
+  echo Device disconnected. Restarting service.
+  goto :start
+:end
 
 :error
 echo Failed with error #%errorlevel%.
